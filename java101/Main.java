fa89650380e6929d96ebd57b30d590a8a1c5236a
Aaron <aaronlin1001@gmail.com>
2024-11-15 17:31:40 -0500

    Practice problem 2 coded and tested

--- java101/Main.java
+++ java101/Main.java
@@ -2,7 +2,7 @@ package java101;
 
 public class Main {
     public static void main(String[] args) {
-        int [] arr = {3, 2, 1, 3, 2, 1, 3, 3,};
-        System.out.println(PracticeProblems.countOccurrences(1, arr));
+        int [] arr = {2, 1, 2, 3, 4, 5, 6, 7, 8, 8};
+        System.out.println(PracticeProblems.reversedArray(arr));
     }
 }
--- java101/PracticeProblems.java
+++ java101/PracticeProblems.java
@@ -10,4 +10,15 @@ public class PracticeProblems {
         }
         return count;
     } 
+    static String reversedArray(int[] arr) {
+        String str = "{";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                str += (arr[i]) + ", "; 
+        }   else {
+            str += (arr[i]);
+            }
+        }
+    return str += "}";
+    }
 }
