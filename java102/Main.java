b27868afeb75cb3c0d1903d9ec10c7ecaa4166e0
Aaron <aaronlin1001@gmail.com>
2024-11-26 00:03:11 -0500

    did the next 2 practice for java 102

--- java102/Main.java
+++ java102/Main.java
@@ -1,22 +1,14 @@
 package java102;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Point point = new Point(4, 3);  
         Point a = new Point(2, 1);
         Point b = new Point(5, 10);
 
-        System.out.println("x: " + point.x + "y: " + point.y);
-
-        Point xTranslation = point.translateX(3);
-        Point yTranslation = point.translateY(-7); 
-        Point xyTranslation = point.translateX(-4).translateY(-3); 
-
-        System.out.println("x transl: " + xTranslation);
-        System.out.println("y transl: " + yTranslation);
-        System.out.println("xy transl: " + xyTranslation);
-
-        System.out.println("point: " + point);
+        System.out.println(point.x);
 
         System.out.println(Point.distance(a, b));
 
@@ -45,5 +37,38 @@ public class Main {
         Point angleTest5 = new Point(0, 0);
         angle = angleTest5.angle();
         System.out.println(angle); 
+
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Hello");
+        arr.add("World");
+        System.out.println(arr);
+        arr.set(0, "Goodbye");
+        System.out.println(arr);
+
+        ArrayList<Boolean> conditions = new ArrayList<>();
+        conditions.add(true);
+        conditions.add(arr.get(0) == "Hello");
+        conditions.add(conditions.get(0) || conditions.get(1));
+        System.out.println(conditions);
+
+        ArrayList<Integer> intArr = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+	        intArr.add(0);
+        }
+        System.out.println(intArr.size());
+        System.out.println(intArr.get(32));
+
+        Grid<Integer> grid = new Grid<>(5, 1);
+        grid.set(2, 2, 4);
+        System.out.println(grid);
+
+        String[] arr2 = {"e", "f", "g", "h"};
+        System.out.println(Grid.arrayToString(arr2));
+
+        System.out.println(grid.diagonal());
+
+        Grid<Integer> grid1 = new Grid<>(6, 3);
+        System.out.println(Grid.maxSideLength());
+
     }
 }
